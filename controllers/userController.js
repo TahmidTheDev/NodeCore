432b10d78c66858cfcf5109c29816325c5637670
TahmidTheDev <tahmidwhafi@gmail.com>
2025-07-07 09:06:38 +0600

    implement getallusers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-export const getallusers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route not implement',
+import User from '../models/userModel.js';
+import catchAsync from '../utilis/catchAsync.js';
+
+export const getallusers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 export const getuser = (req, res) => {
   res.status(500).json({
